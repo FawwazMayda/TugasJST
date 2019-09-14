1501989abd362bb979147b6f281ff05c625de431
fawwazmayda <maydafawwaz@gmail.com>
2019-09-14 01:30:59 -0700

    Init weight and forward pass

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -6,5 +6,36 @@ class NeuralNetwork():
         self.n_class = n_class
         self.input_shape = input_shape
         self.hidden_layers = hidden_layers
+        self.all_layers = self.hidden_layers.copy().append(n_class).append(self.input_shape,0)
+        self.weights = []
+        self.bias = []
+        self.dw = []
+        self.db = []
+        self.a = [] #layers output after activation
+        self.z = [] #layers output before activation
 
-    def init_network():
+        for layer1,layer2 in zip(self.all_layers[:-1], self.all_layers[1:]):
+            wl = (layer1,layer2)
+            bl = (layer2,1)
+            self.weights.append(np.random.normal(wl))
+            self.dw.append(np.zeros(wl))
+            self.bias.append(np.random.normal(bl))
+            self.db.append(np.zeros(bl))
+    
+
+    def sigmoid(self,x):
+        return 1/(1 + np.exp(-x))
+    
+
+    def forward(self,X):
+        self.a = [X.T]
+        self.z = [None]
+
+        for (weight,bias) in zip(self.weights,self.bias):
+            self.z.append(np.dot(self.a[-1],weight) + bias)
+            self.a.append(sigmoid(self.z.[-1]))
+        return self.a[-1].T
+
+
+    def backward(self):
+

83d48c91e18643f945f97753e87d3b0768987002
fawwazmayda <maydafawwaz@gmail.com>
2019-09-14 00:58:55 -0700

    Hidden layer

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -2,8 +2,9 @@ import numpy as np
 
 class NeuralNetwork():
 
-    def __init__(self,n_class,input_shape):
+    def __init__(self,n_class,input_shape,hidden_layers):
         self.n_class = n_class
         self.input_shape = input_shape
+        self.hidden_layers = hidden_layers
 
     def init_network():

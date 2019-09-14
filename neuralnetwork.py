47d5967ab4b99f21f4aaad1d0fdaa34e468f4b4d
fawwazmayda <maydafawwaz@gmail.com>
2019-09-14 00:48:18 -0700

    NeuralNets Module

---
+++ neuralnetwork.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+class NeuralNetwork():
+
+    def __init__(self,n_class,input_shape):
+        self.n_class = n_class
+        self.input_shape = input_shape
+
+    def init_network():
